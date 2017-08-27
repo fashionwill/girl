1d4894f73ae9c276f9d2b0460a0a6c3868340cea
1282988455@qq.com <1282988455@qq.com>
2017-08-27 12:55:36 +0800

    loaddata

--- app/portal/controller/IndexController.php
+++ app/portal/controller/IndexController.php
@@ -18,9 +18,21 @@ class IndexController extends HomeBaseController
     public function index()
     {
     	/*获取数据库数据*/
-    	//Db::name('portal_post')->where(['id' => $articleId])->setInc('post_hits');
     	$result=Db::name('face')->select();
     	print_r($result);
        // return $this->fetch(':index');
     }
+    /*ajax获取数据*/
+    public function loaddata()
+    {
+       // 查询状态为1的用户数据 并且每页显示10条数据
+    	echo $page=$this->request->param('p', 1, 'intval');
+    	echo $type=$this->request->param('type', 1, 'intval');
+
+		$list = Db::name('face')->paginate(1,$page);
+		// 把分页数据赋值给模板变量list
+		$this->assign('list', $list);
+		print_r($list);
+    }
+
 }
